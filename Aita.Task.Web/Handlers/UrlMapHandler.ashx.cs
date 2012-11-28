aa07d03ec7f572d8ac74f8d6f9dbee4e05f9c701
xuehua <txh_723@163.com>
2012-11-28 16:51:20 +0800

    add create task page.

--- Aita.Task.Web/Handlers/UrlMapHandler.ashx.cs
+++ Aita.Task.Web/Handlers/UrlMapHandler.ashx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Net;
 using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
 
 namespace Aita.Task.Web.Handlers
 {
@@ -15,13 +18,121 @@ namespace Aita.Task.Web.Handlers
         public void ProcessRequest(HttpContext context)
         {
             var url = HttpContext.Current.Request.QueryString["url"];
+            var parameters = GetParameters(context.Request);
 
-            using (var wc = new WebClient() { Encoding = Encoding.UTF8 })
+            Debug.WriteLine("----Parameters:");
+            foreach (var parameter in parameters)
             {
-                var json = wc.DownloadString(url);
-                context.Response.ContentType = "text/plain";
-                context.Response.Write(json);
+                Debug.WriteLine(parameter.Key, parameter.Value);
             }
+
+            var result = PostData(url, parameters);
+
+            Debug.WriteLine("----Result:");
+            Debug.Write(result);
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(result);
+        }
+
+        /// <summary>
+        /// Send a http post request to a given url with given data.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string PostData(string url, IDictionary<string, object> data)
+        {
+            string content = "";
+
+            //send http post request.
+            ServicePointManager.ServerCertificateValidationCallback = (srvPoint, certificate, chain, errors) => true;
+            var request = HttpWebRequest.Create(url) as HttpWebRequest;
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            //set post data
+            var parameters = new StringBuilder();
+            foreach (var item in data)
+            {
+                parameters.Append("&" + string.Format("{0}={1}", item.Key, item.Value != null ? HttpUtility.UrlEncode(item.Value.ToString()) : null));
+            }
+            var payload = Encoding.UTF8.GetBytes(parameters.ToString().Substring(1));
+            request.ContentLength = payload.Length;
+
+            using (var requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(payload, 0, payload.Length);
+                requestStream.Close();
+            }
+
+            //Send request
+            var response = request.GetResponse() as HttpWebResponse;
+
+            //read response into memory stream
+            MemoryStream memoryStream;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                memoryStream = new MemoryStream();
+
+                byte[] buffer = new byte[1024];
+                int byteCount;
+                do
+                {
+                    byteCount = responseStream.Read(buffer, 0, buffer.Length);
+                    memoryStream.Write(buffer, 0, byteCount);
+                } while (byteCount > 0);
+            }
+
+            //get result content from memory stream.
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var encoding = Encoding.UTF8;
+            var charset = response.CharacterSet;
+            if (charset != null)
+            {
+                encoding = Encoding.GetEncoding(charset);
+            }
+            using (var streamReader = new StreamReader(memoryStream, encoding))
+            {
+                content = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            return content;
+        }
+        private IDictionary<string, object> GetParameters(HttpRequest request)
+        {
+            if (request.HttpMethod == "POST")
+            {
+                var json = GetClientPostData(request.InputStream);
+                var serializer = new JavaScriptSerializer();
+                var parameters = serializer.Deserialize<Dictionary<string, object>>(json);
+                return parameters;
+            }
+            else
+            {
+                IDictionary<string, object> parameters = new Dictionary<string, object>();
+                foreach (var key in request.Params.Keys)
+                {
+                    parameters.Add(key.ToString(), request.Params[key.ToString()]);
+                }
+                return parameters;
+            }
+        }
+        private string GetClientPostData(Stream stream)
+        {
+            var content = string.Empty;
+            stream.Seek(0, SeekOrigin.Begin);
+            var encoding = Encoding.UTF8;
+            using (var streamReader = new StreamReader(stream, encoding))
+            {
+                content = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+
+            return content;
         }
 
         public bool IsReusable
---
+++ Aita.Task.Web/Scripts/controllers/createTask.js
@@ -0,0 +1,82 @@
+﻿
+function CreateTaskCtrl($scope, $rootScope, $http, $location, $routeParams, $element, urls, userId) {
+
+    //初始化页面
+    function initPage(params) {
+        $('#main').show();
+
+        $scope.innerFindUserUrl = 'http://localhost:9000/sys/autoprompt';
+        $scope.innerCreateTaskUrl = 'http://localhost:9000/EnterpriseTask/CreateTask';
+
+        $scope.findUserUrl = urls.map_url + '?url=' + $scope.innerFindUserUrl + '&_=' + new Date().getTime();
+        $scope.createTaskUrl = urls.map_url + '?url=' + $scope.innerCreateTaskUrl + '&_=' + new Date().getTime();
+
+        $scope.subject = null;
+        $scope.body = null;
+        $scope.priority = 0;
+        $scope.dueTime = null;
+
+        //负责人
+        $("#assigneeSelector").Selector(
+        {
+            dropListUrl: $scope.findUserUrl,
+            ifRepeat: true,
+            maxDrop: 11,
+            maxToken: 999,
+            initData: [{ id: '10000', name: '何望' }, { id: '10001', name: '侯昆'}]
+        });
+
+        //相关人员
+        $("#relatedSelector").Selector(
+        {
+            dropListUrl: $scope.findUserUrl,
+            ifRepeat: true,
+            maxDrop: 11,
+            maxToken: 999,
+            initData: [{ id: '10000', name: '何望' }, { id: '10004', name: '萧玄'}]
+        });
+
+        //优先级
+        $("#priority button").click(function (e) {
+            $scope.priority = $(this).val();
+            $('#priority button').removeClass('active');
+            $(this).toggleClass("active");
+        });
+
+        //期望完成时间
+        $('#dueTime').datepicker({ format: 'yyyy-mm-dd' })
+            .on('changeDate', function (e) {
+                var value = e.date.valueOf();
+                $scope.dueTime = new Date(value);
+                $('#dueTime').datepicker('hide');
+        });
+
+        $scope.saveTask = function () {
+            var data = {
+                userId: userId,
+                subject: $scope.subject,
+                body: $scope.body,
+                dueTime: $scope.dueTime,
+                priority: $scope.priority,
+                assigneeUserId: $("#assigneeUserId").val(),
+                relatedUserJson: $("#relatedUserIds").val()
+            };
+
+            $http.post($scope.createTaskUrl, data).success(function (data, status, headers, config) {
+                if (data.state == 0) {
+                    alert('任务创建成功，任务ID:' + data.data);
+                    window.location = "GetTasksByCreator.htm?userId=" + userId + "&key=&isAssignee=false&isAssignedToOther=false&externalTaskSourceJson=";
+                }
+                else {
+                    alert(data.data);
+                }
+            });
+        };
+        $scope.cancel = function () {
+            //TODO, 重定向到任务列表页面
+            alert("task create canceled, redirect to task list page.");
+        };
+    }
+
+    initPage();
+}
